b07d402e669f4947db75ec2e19dddfb239d0536e
jason32 <166226090+SongJiang32@users.noreply.github.com>
2025-10-23 21:05:21 +0800

    Delete jason_ws/build directory

--- jason_ws/build/jason_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jason32/jason_ws/src/jason_control
-# Build directory: /home/jason32/jason_ws/build/jason_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
